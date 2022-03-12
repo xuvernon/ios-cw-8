7d05ba53fcf813299d450e73ea799ea2eba1d010
sarah almutari <sara0moha78@gmail.com>
2022-03-12 11:02:37 +0300

    .

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -2,4 +2,14 @@ import UIKit
 
 // التمرين الأول
 // ClassWork 1
+var name = "sarah"
+var birthYear = 2005
+var age = 2022-birthYear
+    print("helllo my name is \(name) and my age is \(age)" )
 
+
+var num1 = "8"
+var num2 = 4
+var num3 = 2.5
+
+num3 = Double(num1) ?? 0 + Double(num2) 
